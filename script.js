29cd9a9382f5466e31b602d39cbbd8f0e6c0ca98
brihanPenayo <116458646+brihanPenayo@users.noreply.github.com>
2022-12-29 16:22:28 -0300

    Agregando atributo ALT a la busqueda

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const API_KEY = "insert_API_here";
+random();
 
 $('#btnSearch').click(function () {
-    // $('#resultado').html("");
+    // $('#resultado').html(""); // Descomentar esto si prefieres que en cada busqueda solo aparezcan los resultados de la busqueda actual
     let busqueda = $('#buscar').val().replaceAll(" ", "+");
     let url = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${busqueda}&limit=8&offset=0&rating=g&lang=es`;
     if (busqueda == "") {
@@ -12,7 +13,7 @@ $('#btnSearch').click(function () {
             url: url,
         }).done(function (result) {
             for (const img of result.data) {
-                $('#resultado').prepend(`<img src="${img.images.fixed_width.url}">`);
+                $('#resultado').prepend(`<img src="${img.images.fixed_width.url}" alt="${img.title}">`);
             }
         });
     }
@@ -34,5 +35,3 @@ function random() {
         });
     }
 }
-
-random();
